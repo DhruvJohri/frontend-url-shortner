91a3e9433ec3f51649583585a7c001fa78e8124f
Dhruv Johri <111191087+DhruvJohri@users.noreply.github.com>
2025-09-06 12:05:39 +0530

    Update routeTree.js

--- src/routing/routeTree.js
+++ src/routing/routeTree.js
@@ -1,16 +1,17 @@
-import { createRootRoute } from "@tanstack/react-router"
-import { homePageRoute } from "./homepage"
-import { authRoute } from "./auth.route"
-import { dasboardRoute } from "./dashboard"
-import RootLayout from "../RootLayout"
+import { createRootRoute } from "@tanstack/react-router";
+import { homePageRoute } from "./homepage";
+import { authRoute } from "./auth.route";
+import { dasboardRoute } from "./dashboard";
+import RootLayout from "../RootLayout";
+import { redirectRoute } from "./redirect.route"; // <-- import the new route
 
 export const rootRoute = createRootRoute({
     component: RootLayout
-})
+});
 
-export const routeTree =rootRoute.addChildren([
-    homePageRoute, 
-    authRoute, 
-    dasboardRoute
-])
-
+export const routeTree = rootRoute.addChildren([
+    homePageRoute,
+    authRoute,
+    dasboardRoute,
+    redirectRoute // <-- add the new route here
+]);
